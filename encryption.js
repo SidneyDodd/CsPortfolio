278587f85f6bee37495301ca6e8ae0833fb1ccb8
Sidney Dodd <sidney_dodd@yahoo.com>
2023-08-08 04:08:52 -0400

    Added salting function for additional security

--- encryption.js
+++ encryption.js
@@ -1,9 +1,15 @@
 const crypto = require('crypto');
 
+// Function to generate a random salt
+function generateSalt() {
+  return crypto.randomBytes(16).toString('hex');
+}
+
 // Function to encrypt a password using Node.js crypto module
 function encryptPassword(password) {
   const algorithm = 'aes-256-gcm';
-  const key = crypto.randomBytes(32);
+  const salt = generateSalt();
+  const key = crypto.scryptSync(password, salt, 32);
   const iv = crypto.randomBytes(16);
 
   const cipher = crypto.createCipheriv(algorithm, key, iv);
@@ -14,17 +20,18 @@ function encryptPassword(password) {
 
   return {
     encryptedPassword,
+    salt,
     iv: iv.toString('hex'),
     authTag: authTag.toString('hex'),
-    key: key.toString('hex')
   };
 }
 
 // Function to decrypt an encrypted password using Node.js crypto module
-function decryptPassword(encryptedData) {
+function decryptPassword(encryptedData, password) {
   const algorithm = 'aes-256-gcm';
 
-  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(encryptedData.key, 'hex'), Buffer.from(encryptedData.iv, 'hex'));
+  const key = crypto.scryptSync(password, encryptedData.salt, 32)
+  const decipher = crypto.createDecipheriv(algorithm, key, Buffer.from(encryptedData.iv, 'hex'), Buffer.from(encryptedData.iv, 'hex'));
   decipher.setAuthTag(Buffer.from(encryptedData.authTag, 'hex'));
 
   let decryptedPassword = decipher.update(encryptedData.encryptedPassword, 'hex', 'utf8');
@@ -39,5 +46,5 @@ const encryptedData = encryptPassword(password);
 
 console.log('Encrypted Password:', encryptedData.encryptedPassword);
 
-const decryptedPassword = decryptPassword(encryptedData);
+const decryptedPassword = decryptPassword(encryptedData, password);
 console.log('Decrypted Password:', decryptedPassword);
